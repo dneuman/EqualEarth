27e904cf114115db77e20835621a8b13e389a0c0
Dan Neuman <659878+dneuman@users.noreply.github.com>
2018-08-14 00:36:03 -0400

    Formatting

--- EqualEarth.py
+++ EqualEarth.py
@@ -5,26 +5,32 @@ Equal Earth Projection
 ======================
 
 Based on code from:
-    https://matplotlib.org/gallery/misc/custom_projection.html
+
+* https://matplotlib.org/gallery/misc/custom_projection.html
+
 and projection described by Bojan Šavrič (@BojanSavric):
-    https://doi.org/10.1080/13658816.2018.1504949
-    https://www.researchgate.net/publication/326879978_The_Equal_Earth_map_projection
+
+* https://doi.org/10.1080/13658816.2018.1504949
+* https://www.researchgate.net/publication/326879978_The_Equal_Earth_map_projection
+
 as well as code from @mbostock:
-    https://beta.observablehq.com/@mbostock/equal-earth-projection
+
+* https://beta.observablehq.com/@mbostock/equal-earth-projection
 
 Abstract:
     "The Equal Earth map projection is a new equal-area pseudocylindrical
-     projection for world maps. It is inspired by the widely used Robinson
-     projection, but unlike the Robinson projection, retains the relative size
-     of areas. The projection equations are simple to implement and fast to
-     evaluate. Continental outlines are shown in a visually pleasing and
-     balanced way."
+    projection for world maps. It is inspired by the widely used Robinson
+    projection, but unlike the Robinson projection, retains the relative size
+    of areas. The projection equations are simple to implement and fast to
+    evaluate. Continental outlines are shown in a visually pleasing and
+    balanced way."
 
 Usage
 -----
 Importing the module causes the Equal Earth projection to be registered with
 Matplotlib so that it can be used when creating a subplot::
 
+    >>>import matplotlib.pyplot as plt
     >>>import EqualEarth
     >>>fig = plt.figure('Equal Earth Projection')
     >>>fig.clear()
